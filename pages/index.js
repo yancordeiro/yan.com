a6495f23fb85f4295eb62ada957c4fb28814d1f7
yancordeiro <47543105+yancordeiro@users.noreply.github.com>
2022-08-29 17:18:02 -0300

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
         <meta />
         <html lang="pt-BR">
           <meta charset="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
